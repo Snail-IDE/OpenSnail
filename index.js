adbb6f4bbbfd8cba3b88085c0cc3ccf7024abedd
nmsderp <130254323+nmsderp@users.noreply.github.com>
2024-02-03 17:03:09 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const port = 3000;
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Route handler for the root URL ("/")
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
