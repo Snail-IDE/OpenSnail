eb33e34b4af125e07353f2fda9c4940499b796f2
nmsderp <130254323+nmsderp@users.noreply.github.com>
2024-02-03 17:05:56 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// Serve static files from the public directory
-app.use(express.static(path.join(__dirname, 'public')));
+// Serve static files from the root directory
+app.use(express.static(__dirname));
 
 // Route handler for the root URL ("/")
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(port, () => {
